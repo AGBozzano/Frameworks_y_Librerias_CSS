2b81f98f27ce89e27a40f40d3b04ab4a354c51ab
Alejandro Bozzano <ag.bozzano@gmail.com>
2017-11-23 01:47:56 -0300

    Terminado todos los proyectos

--- js/app.js
+++ js/app.js
@@ -1,1 +1,12 @@
 $(document).foundation()
+
+$(document).ready(function(){
+
+  $('.button-collapse').sideNav('show');
+  // Hide sideNav
+  $('.button-collapse').sideNav('hide');
+  // Destroy sideNav
+  $('.button-collapse').sideNav('destroy');
+        
+});
+
